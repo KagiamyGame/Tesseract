6c2f2c0b684650ed250f0fa283d7c785afb0fac7
RateekMCPE <rateek34@gmail.com>
2017-02-02 21:35:04 +0100

    Added default port (19132)
    From Genesys.

--- src/pocketmine/network/protocol/TransferPacket.php
+++ src/pocketmine/network/protocol/TransferPacket.php
@@ -7,7 +7,7 @@ class TransferPacket extends DataPacket{
     const NETWORK_ID = Info::TRANSFER_PACKET;
 
     public $address;
-    public $port;
+    public $port = 19132; //default port
 
     public function decode(){
 
